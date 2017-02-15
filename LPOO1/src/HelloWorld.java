f3be403eb32827520398570f65eb2f7cce47a883
luigicorreia <luis.mcorreia97@gmail.com>
2017-02-15 08:30:00 +0000

    :>

---
+++ LPOO1/src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		DnD d = new DnD();
+		
+		
+		d.moveRight();
+		d.moveRight();
+		d.moveDown();
+		d.moveDown();
+		d.moveLeft();
+		d.moveDown();
+		d.moveDown();
+		d.moveUp();
+		d.printBoard();
+
+	}
+
+}
