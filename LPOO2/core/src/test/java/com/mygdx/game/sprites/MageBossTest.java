2fb6a209232fd771bb986a8222e36cd1539594ff
luigicorreia <luis.mcorreia97@gmail.com>
2017-06-06 10:39:20 +0100

    mage boss done, test it boii, also tried to unit test

--- LPOO2/core/src/test/java/com/mygdx/game/sprites/MageBossTest.java
+++ LPOO2/core/src/test/java/com/mygdx/game/sprites/MageBossTest.java
@@ -1,8 +1,31 @@
+package com.mygdx.game.sprites;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.screens.PlayScreen;
+import com.mygdx.game.tools.Trigger;
+
+import org.junit.Test;
+
+import test.GameTest;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Luís on 05/06/2017.
  */
-public class MageBossTest {
+public class MageBossTest{
+
+  @Test
+    public void createMageboss(){
+
+    MyGdxGame game = new MyGdxGame();
+
+    PlayScreen screen = new PlayScreen(game);
+
+    Trigger trigger = new Trigger(screen,2,2);
+
+    assertFalse(trigger.isDestroyed());
+  }
 
 }
